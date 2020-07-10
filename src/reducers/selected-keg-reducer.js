6e609e7e143b985582c3ee506f576740e5f2f9cf
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-07-10 15:25:20 -0700

    continue attemptint selectKeg to work

--- src/reducers/selected-keg-reducer.js
+++ src/reducers/selected-keg-reducer.js
@@ -1,15 +1,18 @@
 import * as c from '../../src/actions/ActionTypes';
-
+import { store } from '../index.js';
 export default (state = {}, action) => {
   const { id } = action;
   switch (action.type) {
     case c.SELECT_KEG:
-      const selectedKeg = id;
+      console.log("hi");
+      console.log(store.state.masterKegList);
+      const newState = { ...state };
+      const selectedKeg = newState[id];
       console.log(selectedKeg);
       return selectedKeg;
     case c.DESELECT_KEG:
       console.log("hi");
-      const thisState = null;
+      const thisState = {};
       return thisState;
     default:
       return state;
